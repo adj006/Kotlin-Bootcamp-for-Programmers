e7f500fc09284cb06e966439567bca080b1d3b16
Adrian <ajim1989@gmail.com>
2019-11-18 15:50:13 -0800

    Completed Higher Order Functions

---
+++ src/Lesson6/Lesson6.kt
@@ -0,0 +1,36 @@
+package Lesson6
+
+data class Fish (var name: String)
+
+fun main(args: Array<String>) {
+    fishExamples()
+}
+
+fun fishExamples() {
+    val fish = Fish("splashy")
+
+    /*
+    with (fish.name) {
+        capitalize()
+    }
+    */
+    myWith (fish.name) {
+        println(capitalize())
+    }
+
+    println(fish.run { name })
+
+    println(fish.apply {  })
+
+    val fish2 = Fish("splashy").apply { name = "Sharky" }
+    println(fish2.name)
+
+    println(fish.let { it.name.capitalize() }
+        .let { it + "fish" }
+        .let { it.length }
+        .let { it + 31 })
+}
+
+fun myWith(name: String, block: String.() -> Unit) {
+    name.block()
+}
